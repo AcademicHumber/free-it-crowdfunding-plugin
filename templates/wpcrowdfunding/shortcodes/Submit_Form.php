c826a6fe77f9ffb56b06c9d0f13b8a86d072de0b
Humber <ahfernandez-es@udabol.edu.bo>
2021-10-28 11:25:53 -0400

    Deleted pro version restrictions on campaign creation form

--- templates/wpcrowdfunding/shortcodes/Submit_Form.php
+++ templates/wpcrowdfunding/shortcodes/Submit_Form.php
@@ -659,17 +659,12 @@ class Campaign_Submit_Form
                 $html .= '</div>';
             }
 
-            if (!wpcf_function()->is_free()) {
-                $html .= '<div style="clear: both;"></div>';
-                if (is_admin()) {
-                    $html .= '<p><i> ' . __('pro version is required to add more than 1 reward', 'wp-crowdfunding') . '. <a href="https://www.themeum.com/product/wp-crowdfunding-plugin/?utm_source=crowdfunding_plugin" target="_blank">' . __('click here to get pro version', 'wp-crowdfunding') . '</a></i></p>';
-                }
-            } else {
-                $html .= '<div id="rewards_addon_fields"></div>';
-                $html .= '<div class="text-right">';
-                $html .= '<input type="button" value="' . __("+ Add", "wp-crowdfunding") . '" id="addreward" class="button tagadd" name="save">';
-                $html .= '</div>';
-            }
+
+            $html .= '<div id="rewards_addon_fields"></div>';
+            $html .= '<div class="text-right">';
+            $html .= '<input type="button" value="' . __("+ Add", "wp-crowdfunding") . '" id="addreward" class="button tagadd" name="save">';
+            $html .= '</div>';
+
 
             $html .= '</div>';
         }
